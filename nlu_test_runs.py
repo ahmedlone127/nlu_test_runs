eda92fe04e53ab7c19b3e6476862e6017ea053e9
ahmedlone127 <66001253+ahmedlone127@users.noreply.github.com>
2020-11-26 01:00:05 +0500

    Update nlu_test_runs.py

--- nlu_test_runs.py
+++ nlu_test_runs.py
@@ -9,9 +9,21 @@ args=parser.parse_args()
 path = args.file
 
 def findOccurrences(s, ch):
+	"""Finds Occurences of a character in a string.
+	
+	Keyword arguments:
+	s -- string
+	ch -- character
+    """
     return [i for i, letter in enumerate(s) if letter == ch]
 
 def get_path(directory,extension):
+	"""Finds all specific type of files in given directory.
+	
+	Keyword arguments:
+	direcotry  -- directory to search file for 
+	extension -- what kind of file to search for
+	"""
 	list_of_path = []
 	for root,dirs,files in os.walk(directory):
 		for file in files:
@@ -20,6 +32,13 @@ def get_path(directory,extension):
 	return list_of_path
 
 def get_last_path(directory,extension,keyword):
+	"""Finds all specific type of files in given directory with a specific keyword in their name .
+	
+	Keyword arguments:
+	direcotry  -- directory to search file for 
+	extension -- what kind of file to search for
+	keyword  -- what keyword should the file name contain 
+	"""
 	list_of_path = []
 	for root,dirs,files in os.walk(directory):
 		for file in files:
@@ -28,11 +47,20 @@ def get_last_path(directory,extension,keyword):
 	return list_of_path
 
 def make_Files(paths):
+	"""Conerts notebooks to python scripts.
+	
+	Keyword arguments:
+	paths   -- path of files to convert 
+	"""
 	for path in paths :
 		os.system(f"jupyter nbconvert '{path}'  --to script")
 
 def edit_files(paths):
-
+	"""Removes some parts of the file to make it runnable
+	
+	Keyword arguments:
+	paths   -- path of files to convert 
+	"""
 	for name in paths:
 		if "done" not in name:
 			fin = open(name, "r+", encoding = "utf-8")
@@ -68,6 +96,11 @@ def edit_files(paths):
 		fin.close()
 
 def run_Files(paths):
+	"""Runs file and saves output to a file 
+	
+	Keyword arguments:
+	paths   -- path of files to run
+	"""
 
 		for path in paths:
 			result_name =path.replace(".done.txt","result.txt")
@@ -80,6 +113,11 @@ def run_Files(paths):
 				fout.write("----------------------------------------------------------------------------------------------------------")
 				fout.close()
 def check_For_Errors(paths):
+	"""Checks whether the output contains any errors and saves erros to a file 
+	
+	Keyword arguments:
+	paths   -- path of files to check errors for 
+	"""
 	fout = open("erros.txt", "a+",encoding= "utf-8")
 	for path in paths :
 		fin = open(path, "r+", encoding = "utf-8")
@@ -101,13 +139,6 @@ def check_For_Errors(paths):
 	for line in lines:
 		print(line)
 	fout.close()
-'''def readfiles(files):
-	for file in files[-2:-1]:
-		fout = open(file,"r+",encoding ="utf-8")
-		lines = fout.readlines()
-		for line in lines:
-			print(line)
-		fout.close()'''
 paths_For_ipynb = get_path(path,".ipynb")	
 make_Files(paths_For_ipynb)
 paths_For_txt = get_path(path,".txt")
@@ -117,4 +148,4 @@ run_Files(paths_of_Files_to_run)
 result_files = get_last_path(path,".txt","result")
 print(result_files)
 check_For_Errors(result_files)
-#readfiles(paths_of_Files_to_run)
+
