98478a7bb0992fdef3eb2983ed04f3a287372da7
ahmedlone127 <66001253+ahmedlone127@users.noreply.github.com>
2020-11-29 00:42:56 +0500

    Update nlu_test_runs.py

--- nlu_test_runs.py
+++ nlu_test_runs.py
@@ -68,7 +68,7 @@ def edit_files(paths):
             fout = open(name.replace("txt","done.txt"), "w+",encoding= "utf-8")
             lines = fin.readlines()
             
-            fout.write("import wget\n".encode('ascii', 'ignore').decode('ascii'))
+            fout.write("import wget".encode('ascii', 'ignore').decode('ascii'))
             
             for line in lines : 
                 if ("wget" in line):#downloads data frame from url 
@@ -81,7 +81,7 @@ def edit_files(paths):
                             path_ = url[index+1]
                     path_ = base[:-1]+path_
                     paths_.append(path_)
-                    fout.write(f"wget.download('{url_}',out= '{path_}' )".encode('ascii', 'ignore').decode('ascii'))
+                    fout.write(f"wget.download('{url_.replace("\n","")}',out= {path_} )".encode('ascii', 'ignore').decode('ascii'))
                 elif ("pd.read_csv" in line):
                     inedex_ = findOccurrences(line,"'")
                     line = line[:inedex_[0]] + base + line[inedex_[0]:]
