b45d55b14b814013c5283922221252fa9bbf1957
ahmedlone127 <66001253+ahmedlone127@users.noreply.github.com>
2020-11-29 02:01:46 +0500

    Update nlu_test_runs.py

--- nlu_test_runs.py
+++ nlu_test_runs.py
@@ -84,7 +84,7 @@ def edit_files(paths):
                     path_= path_.replace(r'\n', "/")+"/"
                    
                     paths_.append(path_)
-                    fout.write(f"wget.download('{url_}',out= '{path_}' )")
+                    fout.write(f"wget.download('{url_}',out=\'{path_}\' )")
                         #.encode('ascii', 'ignore').decode('ascii'))
                 elif ("pd.read_csv" in line):
                     inedex_ = findOccurrences(line,"'")
