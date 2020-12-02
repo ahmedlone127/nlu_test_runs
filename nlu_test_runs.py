8724e9e1348076c154cea99a6016e4e6588137ff
ahmedlone127 <66001253+ahmedlone127@users.noreply.github.com>
2020-12-02 19:09:38 +0500

    Update nlu_test_runs.py

--- nlu_test_runs.py
+++ nlu_test_runs.py
@@ -103,12 +103,12 @@ def run_Files(paths):
         result_name =path.replace(".done.txt","result.txt")
         #  &> '{result_name}'
         try :
-            os.system(f"python3.6 '{path}'  >'{result_path}' 2>&1")
+            os.system(f"python3.6 '{path}'  >'{result_name}' 2>&1")
         except Exception as e:# if it fails write error to file 
             fout = open("errors.txt", "a+",encoding= "utf-8")    
-            fout.write(f"name : {path}".encode('ascii', 'ignore').decode('ascii'))
+            fout.write(f"name : {path}\n".encode('ascii', 'ignore').decode('ascii'))
             fout.write(f"{e}\n".encode('ascii', 'ignore').decode('ascii'))
-            fout.write("----------------------------------------------------------------------------------------------------------".encode('ascii', 'ignore').decode('ascii'))
+            fout.write("----------------------------------------------------------------------------------------------------------\n".encode('ascii', 'ignore').decode('ascii'))
             fout.close()
 
 def check_For_Errors(paths):
