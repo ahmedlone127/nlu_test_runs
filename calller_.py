79e59dddb3d5aed0366c1ffaa4402dff6ca85f2d
ahmedlone127 <66001253+ahmedlone127@users.noreply.github.com>
2021-02-04 21:26:00 +0500

    Create calller_.py

---
+++ calller_.py
@@ -0,0 +1,3 @@
+os.sytem("python3.6 test_nlu_embed.py --file 'tripadvisor_hotel_reviews.csv' --text 'text' --y_col 'y' --iloc '100' --datatype 'classifier' --embed 'en.embed_sentence.small_bert_L2_128'")
+os.sytem("python3.6 test_nlu_embed.py --file 'tripadvisor_hotel_reviews.csv' --text 'text' --y_col 'y' --iloc '100' --datatype 'classifier' --embed 'en.embed_sentence.small_bert_L4_128'")
+os.sytem("python3.6 test_nlu_embed.py --file 'tripadvisor_hotel_reviews.csv' --text 'text' --y_col 'y' --iloc '100' --datatype 'classifier' --embed 'en.embed_sentence.small_bert_L6_128'")
