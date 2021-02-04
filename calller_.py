1526003158e2ee00aa389612c590de6c00ed822f
ahmedlone127 <66001253+ahmedlone127@users.noreply.github.com>
2021-02-04 22:01:25 +0500

    Update calller_.py

--- calller_.py
+++ calller_.py
@@ -1,4 +1,4 @@
 import os 
-os.sytem("python3.6 nlu_training_multi_class_text_classifier_demo_hotel_reviews.py --file 'tripadvisor_hotel_reviews.csv' --text 'text' --y_col 'y' --iloc '100' --datatype 'classifier' --embed 'en.embed_sentence.small_bert_L2_128'")
-os.sytem("python3.6 nlu_training_multi_class_text_classifier_demo_hotel_reviews.py --file 'tripadvisor_hotel_reviews.csv' --text 'text' --y_col 'y' --iloc '100' --datatype 'classifier' --embed 'en.embed_sentence.small_bert_L4_128'")
-os.sytem("python3.6 nlu_training_multi_class_text_classifier_demo_hotel_reviews.py --file 'tripadvisor_hotel_reviews.csv' --text 'text' --y_col 'y' --iloc '100' --datatype 'classifier' --embed 'en.embed_sentence.small_bert_L6_128'")
+os.system("python3.6 nlu_training_multi_class_text_classifier_demo_hotel_reviews.py --file 'tripadvisor_hotel_reviews.csv' --text 'text' --y_col 'y' --iloc '100' --datatype 'classifier' --embed 'en.embed_sentence.small_bert_L2_128'")
+os.system("python3.6 nlu_training_multi_class_text_classifier_demo_hotel_reviews.py --file 'tripadvisor_hotel_reviews.csv' --text 'text' --y_col 'y' --iloc '100' --datatype 'classifier' --embed 'en.embed_sentence.small_bert_L4_128'")
+os.system("python3.6 nlu_training_multi_class_text_classifier_demo_hotel_reviews.py --file 'tripadvisor_hotel_reviews.csv' --text 'text' --y_col 'y' --iloc '100' --datatype 'classifier' --embed 'en.embed_sentence.small_bert_L6_128'")
