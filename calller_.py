65b8842d95fc3daf5efab931edfb77b27544e2be
ahmedlone127 <66001253+ahmedlone127@users.noreply.github.com>
2021-07-08 14:15:47 +0500

    Update calller_.py

--- calller_.py
+++ calller_.py
@@ -2,3 +2,5 @@ import os
 os.system("python3.6 nlu_training_multi_class_text_classifier_demo_hotel_reviews.py --file 'tripadvisor_hotel_reviews.csv' --text 'text' --y_col 'y' --iloc '500' --datatype 'classifier' --embed 'en.embed_sentence.small_bert_L2_128'")
 os.system("python3.6 nlu_training_multi_class_text_classifier_demo_hotel_reviews.py --file 'tripadvisor_hotel_reviews.csv' --text 'text' --y_col 'y' --iloc '500' --datatype 'classifier' --embed 'en.embed_sentence.small_bert_L4_128'")
 os.system("python3.6 nlu_training_multi_class_text_classifier_demo_hotel_reviews.py --file 'tripadvisor_hotel_reviews.csv' --text 'text' --y_col 'y' --iloc '500' --datatype 'classifier' --embed 'en.embed_sentence.small_bert_L6_128'")
+
+
